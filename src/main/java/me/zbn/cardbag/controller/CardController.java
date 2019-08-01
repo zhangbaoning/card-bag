9f564375730e4f50b26608843a23da134959ef43
zhangbaoning <zhangbaoning@vip.qq.com>
2019-08-01 18:17:29 +0800

    添加openid

--- src/main/java/me/zbn/cardbag/controller/CardController.java
+++ src/main/java/me/zbn/cardbag/controller/CardController.java
@@ -8,7 +8,10 @@ import me.zbn.cardbag.service.RelationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,11 +30,11 @@ public class CardController {
     private CardService service;
     @Autowired
     private RelationService relationService;
+    Gson gson = new Gson();
 
     @PostMapping("upload")
     @ResponseBody
     public Map<String, String> uploadCard(String openid, MultipartFile file) {
-        Gson gson = new Gson();
         Map<String, String> returnMap = new HashMap<>(1);
         try {
             // 获取格式
@@ -50,7 +53,7 @@ public class CardController {
                 service.save(bankCard);
                 returnMap.put("imgUrl", imgUrl);
 
-                relationService.save(openid, uuid+ formart, bankCard.getCardNo(),"bankcard");
+                relationService.save(openid, uuid + formart, bankCard.getCardNo(), "bankcard");
             } else {
                 //通过卡号返回UUID
                 uuid = relationService.getByCardNo(bankCard.getCardNo()).getUuid();
@@ -68,38 +71,41 @@ public class CardController {
         }
         return returnMap;
     }
+
     @GetMapping("getAllCard/{openid}")
     @ResponseBody
-    public List getCardList(@PathVariable("openid") String openid){
+    public List getCardList(@PathVariable("openid") String openid) {
         // TODO openid
-       List<Relation> relationList =relationService.getAll();
-       List returnList = new ArrayList();
+        List<Relation> relationList = relationService.getAll();
+        List returnList = new ArrayList();
         for (Relation relation : relationList) {
             Map relationMap = new HashMap(2);
-           String url =  service.getUrlByFileName(relation.getUuid());
-           int id = relation.getId();
-           relationMap.put("id",id);
-           relationMap.put("url",url);
-           relationMap.put("type","image");
+            String url = service.getUrlByFileName(relation.getUuid());
+            int id = relation.getId();
+            relationMap.put("id", id);
+            relationMap.put("url", url);
+            relationMap.put("type", "image");
             returnList.add(relationMap);
         }
         return returnList;
     }
+
     @ResponseBody
     @GetMapping("onLogin")
-    public void onLogin(String code){
+    public Map<String,Object> onLogin(String code) {
         RestTemplate template = new RestTemplate();
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={js_code}&grant_type={grant_type}";
-        Map paramMap = new HashMap();
-        paramMap.put("js_code",code);
-        paramMap.put("appid","wx6e7ba566cdb6dcf8");
-        paramMap.put("secret","8262a066f8d262c25099aab6106f2ff7");
-        paramMap.put("grant_type","authorization_code");
-        ResponseEntity<String> returnStr = template.getForEntity(url,String.class, paramMap);
-        System.out.println(returnStr.toString());
+        Map<String,String> paramMap = new HashMap<>(4);
+        paramMap.put("js_code", code);
+        paramMap.put("appid", "wx6e7ba566cdb6dcf8");
+        paramMap.put("secret", "8262a066f8d262c25099aab6106f2ff7");
+        paramMap.put("grant_type", "authorization_code");
+        ResponseEntity<String> returnStr = template.getForEntity(url, String.class, paramMap);
 
-        returnStr.getBody();
-        System.out.println(returnStr.getBody());
+        Map returnMap = gson.fromJson(returnStr.getBody(), Map.class);
+        returnMap.remove("session_key");
+        return returnMap;
+
     }
 
 }
